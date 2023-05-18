bc25160495970a2e56f4b81d4f0d5b5dc0a8cd83
Artem Shilov <artemshilov1991@yandex.ru>
2023-05-18 09:39:02 +0400

    add themes and classNames helper

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { Route, Routes } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import { Link } from "react-router-dom";
 import { Suspense } from "react";
 import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
 import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { useTheme } from "./theme/useTheme";
+import classNames from "./helpers/classNames";
 
 function App() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={classNames("app", {}, [theme])}>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
+
+      <button onClick={toggleTheme}>TOGGLE</button>
 
       <Suspense fallback={"...Loading"}>
         <Routes>
---
+++ src/helpers/classNames/index.tsx
@@ -0,0 +1,13 @@
+type Mods = Record<string, string | boolean>;
+
+const classNames = (cls: string, mods: Mods, additional: string[]): string => {
+  return [
+    cls,
+    ...additional,
+    ...Object.entries(mods)
+      .filter((_className, value) => Boolean(value))
+      .map((className) => className),
+  ].join(' ');
+};
+
+export default classNames;
