1630511a1f37326949a96c5aab37eaba9d92a05e
Artem Shilov <artshilov1991@yandex.ru>
2024-07-22 16:24:10 +0400

    fix ci/cd

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,14 +1,14 @@
 import React, {
-  ReactNode,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import { useTheme } from "app/providers/ThemeProvider";
-import styles from "./Modal.module.scss";
-import classNames from "shared/lib/classNames/classNames";
-import Portal from "../Portal/Portal";
+    ReactNode,
+    useCallback,
+    useEffect,
+    useRef,
+    useState,
+} from 'react';
+import { useTheme } from 'app/providers/ThemeProvider';
+import classNames from 'shared/lib/classNames/classNames';
+import styles from './Modal.module.scss';
+import Portal from '../Portal/Portal';
 
 interface ModalProps {
   className?: string;
@@ -20,63 +20,65 @@ interface ModalProps {
 const ANIMATION_DELAY = 300;
 
 const Modal = (props: ModalProps) => {
-  const { className, children, isOpen, onClose } = props;
+    const {
+        className, children, isOpen, onClose,
+    } = props;
 
-  const [isClosing, setIsClosing] = useState(false);
-  const timerRef = useRef<ReturnType<typeof setTimeout>>();
-  const { theme } = useTheme();
+    const [isClosing, setIsClosing] = useState(false);
+    const timerRef = useRef<ReturnType<typeof setTimeout>>();
+    const { theme } = useTheme();
 
-  const closeHandler = useCallback(() => {
-    if (onClose) {
-      setIsClosing(true);
-      timerRef.current = setTimeout(() => {
-        onClose();
-        setIsClosing(false);
-      }, ANIMATION_DELAY);
-    }
-  }, [onClose]);
+    const closeHandler = useCallback(() => {
+        if (onClose) {
+            setIsClosing(true);
+            timerRef.current = setTimeout(() => {
+                onClose();
+                setIsClosing(false);
+            }, ANIMATION_DELAY);
+        }
+    }, [onClose]);
 
-  const onKeyDown = useCallback(
-    (e: KeyboardEvent) => {
-      if (e.key === "Escape") {
-        closeHandler();
-      }
-    },
-    [closeHandler]
-  );
+    const onKeyDown = useCallback(
+        (e: KeyboardEvent) => {
+            if (e.key === 'Escape') {
+                closeHandler();
+            }
+        },
+        [closeHandler],
+    );
 
-  const onContentClick = (e: React.MouseEvent) => {
-    e.stopPropagation();
-  };
-
-  useEffect(() => {
-    if (isOpen) {
-      window.addEventListener("keydown", onKeyDown);
-    }
-
-    return () => {
-      clearTimeout(timerRef.current);
-      window.removeEventListener("keydown", onKeyDown);
+    const onContentClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
     };
-  }, [isOpen, onKeyDown]);
 
-  const mods: Record<string, boolean> = {
-    [styles.opened]: isOpen,
-    [styles.isClosing]: isClosing,
-    [styles[theme]]: true,
-  };
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
 
-  return (
-    <Portal>
-      <div className={classNames(styles.Modal, mods, [className])}>
-        <div className={styles.overlay} onClick={closeHandler}>
-          <div className={styles.content} onClick={onContentClick}>
-            {children}
-          </div>
-        </div>
-      </div>
-    </Portal>
-  );
+        return () => {
+            clearTimeout(timerRef.current);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [isOpen, onKeyDown]);
+
+    const mods: Record<string, boolean> = {
+        [styles.opened]: isOpen,
+        [styles.isClosing]: isClosing,
+        [styles[theme]]: true,
+    };
+
+    return (
+        <Portal>
+            <div className={classNames(styles.Modal, mods, [className])}>
+                <div className={styles.overlay} onClick={closeHandler} role="button" tabIndex={0}>
+                    <div className={styles.content} onClick={onContentClick} role="button" tabIndex={0}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </Portal>
+    );
 };
 
 export default Modal;
