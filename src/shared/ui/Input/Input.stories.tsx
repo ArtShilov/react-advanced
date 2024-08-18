1a4b285a73957a30e8aab74aa74903c4510b4a9f
Artem Shilov <artshilov1991@yandex.ru>
2024-08-18 20:00:55 +0400

    Input fixes

--- src/shared/ui/Input/Input.stories.tsx
+++ src/shared/ui/Input/Input.stories.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Input from './Input';
 
 export default {
     title: 'shared/Input',
     component: Input,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
 } as ComponentMeta<typeof Input>;
 
 const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
-    placeholder: 'test placeholder',
+export const Primary = Template.bind({});
+Primary.args = {
+    placeholder: 'Type text',
+    value: '123123',
 };
